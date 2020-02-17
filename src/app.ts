4913f8e4242d90fe2938065c16bd343fd096a1ae
Valentin Richard <contact@valentin-richard.com>
2020-02-17 09:30:18 +0100

    add support for nodejs and browser env on app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,40 @@
 import { helloWorld } from './core/hello-world';
 
-window.onload = () => {
-  helloWorld()
-};
+export function start(mainCallBack: () => (Promise<any> | any)) {
+  const ENVIRONMENT: 'browser' | 'nodejs' = ('window' in globalThis) ? 'browser' : 'nodejs';
 
+  const run = () => {
+    return new Promise<void>(resolve => resolve(mainCallBack()))
+      .catch((error: any) => {
+        switch (ENVIRONMENT) {
+          case 'browser':
+            process.stdout.write('\x1b[31m');
+            console.log(
+              `[ERROR]`,
+              (typeof error.toJSON === 'function')
+                ? error.toJSON()
+                : error
+            );
+            process.stdout.write('\x1b[0m');
+            break;
+          case 'nodejs':
+            console.error(error);
+            break;
+        }
+      });
+  };
 
+  switch (ENVIRONMENT) {
+    case 'browser':
+      window.onload = run;
+      break;
+    case 'nodejs':
+      run();
+      break;
+  }
+}
+
+start(helloWorld);
 
 
 
