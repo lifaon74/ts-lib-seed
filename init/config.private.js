c986d5b07f1aed61cc1f6f3ff9d2ed0c0f5f20d8
valentin <valentin.richard@informaniak.com>
2021-04-10 17:47:42 +0200

    - fix init seed

--- init/config.private.js
+++ init/config.private.js
@@ -1,8 +1,8 @@
 // my personal config
 module.exports = {
-  githubPage: 'lifaon74/rx-dom',
-  packageName: '@lifaon/rx-dom',
-  libName: 'rx-dom',
+  githubPage: 'lifaon74/rx-js-light-shortcuts',
+  packageName: '@lifaon/rx-js-light-shortcuts',
+  libName: 'rx-js-light-shortcuts',
   author: 'Valentin Richard',
 };
 
