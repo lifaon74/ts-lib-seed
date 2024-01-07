b35dc3e7e51f5730dace29759bb87c4671219c83
Valentin Richard <valentin.richard@infomaniak.com>
2024-01-07 11:51:41 +0100

    v2.5.1

--- init/config.private.mjs
+++ init/config.private.mjs
@@ -1,10 +1,24 @@
 // my personal config
+// export default {
+//   githubPage: 'lirx-js/unsubscribe',
+//   packageName: '@lirx/unsubscribe',
+//   libName: 'lirx/unsubscribe',
+//   author: 'Valentin Richard',
+// };
+
 export default {
-  githubPage: 'lirx-js/unsubscribe',
-  packageName: '@lirx/unsubscribe',
-  libName: 'lirx/unsubscribe',
+  githubPage: 'lirx-js/async-store',
+  packageName: '@lirx/async-store',
+  libName: 'lirx/async-store',
   author: 'Valentin Richard',
 };
+
+// export default {
+//   githubPage: 'uni-fs/local-node-js',
+//   packageName: '@uni-fs/local-node-js',
+//   libName: 'uni-fs/local-node-js',
+//   author: 'Valentin Richard',
+// };
 
 // export default {
 //   githubPage: 'thingmate/vendor-meross',
