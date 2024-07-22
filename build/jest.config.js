39c0cf758ae8b35856bb024c2cc14e3aab2a5a5e
Valentin Richard <valentin.richard@infomaniak.com>
2024-07-22 12:44:34 +0200

    v2.7.0

--- build/jest.config.js
+++ build/jest.config.js
@@ -6,8 +6,6 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['../'],
   transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
     '^.+\\.tsx?$': [
       'ts-jest',
       {
